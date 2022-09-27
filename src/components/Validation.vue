3aa2893be6a69d3c094de4ecffda2dca7937f108
AndiMajore <andi.majore@googlemail.com>
2022-09-27 19:46:41 +0200

    renamed gvl to graphsimviz

--- src/components/Validation.vue
+++ src/components/Validation.vue
@@ -78,7 +78,7 @@ export default {
       return check;
     },
     getIllustration: function () {
-      return this.$config.STATIC_PATH + "assets/gvl_summary_illustration.png"
+      return this.$config.STATIC_PATH + "assets/graphsimviz_summary_illustration.png"
     },
 
 
