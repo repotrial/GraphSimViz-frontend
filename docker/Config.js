ce08e1cd4ee250b173fc0098e1984b0c676e1890
AndiMajore <andi.majore@googlemail.com>
2022-09-29 17:20:28 +0200

    fixing api path again

--- docker/Config.js
+++ docker/Config.js
@@ -1,7 +1,7 @@
 //TODO replace host
-const HOST = "api.digest-validation.net"
+const HOST = "graphvizsim-api.zbh.uni-hamburg.de"
 export const CONTEXT_PATH="/"
 // export const BACKEND_PORT= "8080"
-export const HOST_URL = "https://" + HOST //+ ":" + BACKEND_PORT
+export const HOST_URL = "http://" + HOST //+ ":" + BACKEND_PORT
 export const PATH_PREFIX= "/"
 export const STATIC_PATH="/static/"
